d0ac5749e9857186c1367c7c138846c89cc49f5d
jeonghyeon <wjdgsu53@naver.com>
2022-04-14 10:52:56 +0900

    couponDAO 추가

---
+++ src/main/java/alcohol/mvc/dao/CouponDAO.java
@@ -0,0 +1,27 @@
+package alcohol.mvc.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import alcohol.mvc.dto.CouponDTO;
+
+public interface CouponDAO {
+
+	/**
+	 * 등록 INSERT INTO COUPON VALUES(?,?,?,?,SYSDATE,SYSDATE+30)
+	 * */
+	public int couponInsert(CouponDTO dto)throws SQLException;
+	
+	/**
+	 * 사용(제거) DELECT FROM COUPON WHERE C_NUMBER =?
+	 * */
+	public int couponDelete(String cNumber)throws SQLException;
+	
+	/**
+	 * 쿠폰목록(select) select * from coupon where U_ID=?
+	 * */
+	public List<CouponDTO> couponAll(String id)throws SQLException;
+	
+
+	
+}
