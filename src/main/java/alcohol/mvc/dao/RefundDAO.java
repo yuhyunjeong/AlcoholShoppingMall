2c948f06a4f6cdac6c24e428d4e3351e5d489395
jeonghyeon <wjdgsu53@naver.com>
2022-04-14 10:40:07 +0900

    refundDAO추가

--- src/main/java/alcohol/mvc/dao/RefundDAO.java
+++ src/main/java/alcohol/mvc/dao/RefundDAO.java
@@ -1,9 +1,32 @@
 package alcohol.mvc.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import alcohol.mvc.dto.RefundDTO;
+
 public interface RefundDAO {
 	/**
-	 * 수빈님 한글 아직도... 깨질까요?
-	 * 안깨집니다!
+	 * 환불하기(등록) INSERT INTO REFUND VALUES(?,orders_no_seq.currval,?,?)
 	 * */
-	public void Test() ;
+	public int refundInsert(RefundDTO dto)throws SQLException;
+	
+	/**
+	 * 환불상태 변경(수정) UPDATE REFUND SET RE_STATUS=? WHERE RE_CODE=?
+	 * */
+	public int refundUpdate(RefundDTO dto)throws SQLException;
+	
+	
+	/**
+	 * 환불상태 표시(select)관리자 SELECT * FROM REFUND
+	 * */
+	public List<RefundDTO> refundAll()throws SQLException;
+	
+	/**
+	 * 환불상태 표시(select)판매자가 보는 거 있어야 하나? 고민중 
+	 * */
+	
+	
+	
+	
 }
